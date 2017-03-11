d365595b5f3b1cd8a22eaf9fba4281b1cac85c90
DhrisyaV <dhrisyav94@gmail.com>
2017-03-11 16:45:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var submit = document.getElementById('submit-btn');
+submit.onclick=function(){
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://dhrisyav.imad.hasura-app.io/login'+ name,true);
+request.send(JSON.stringify({username : username, password : password}));
+}
